2d4377f8398ff8370f6708435da3f7e83f8776f7
Kamil Ciemniewski <info@digitalinventor.pl>
2012-05-10 18:12:24 +0200

    ...

--- app/models/payment_method/bank_transfer.rb
+++ app/models/payment_method/bank_transfer.rb
@@ -4,4 +4,34 @@ class PaymentMethod::BankTransfer < PaymentMethod
     false
   end
 
+  def actions
+    %w{capture void}
+  end
+
+  # Indicates whether its possible to capture the payment
+  def can_capture?(payment)
+    ['checkout', 'pending'].include?(payment.state)
+  end
+  
+  # Indicates whether its possible to void the payment.
+  def can_void?(payment)
+    payment.state != 'void'
+  end
+  
+  def capture(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.complete
+    true
+  end
+  
+  def void(payment)
+    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
+    payment.void
+    true
+  end
+  
+  def source_required?
+    false
+  end
+
 end
